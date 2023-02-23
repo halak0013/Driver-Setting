1d115c4771eaa4c22b2c2b3fbc704c0e0f035b1e
Muhammet <halakmuhammet145@gmail.com>
2023-02-23 22:56:34 +0300

    gnome session bug fixing

--- fonksiyonlar.py
+++ fonksiyonlar.py
@@ -53,7 +53,9 @@ def degistir(mode):
     konum = os.getcwd()
     if goruntu_yoneticisi == "sddm":
         ekle = "--dm sddm"
-    komut = f"""sudo python3 {konum}/envycontrol.py -s {mode} {ekle}
+    komut = f"""#!/usr/bin/bash
+echo 'Lütfen şifrenizi giriniz'
+sudo python3 {konum}/envycontrol.py -s {mode} {ekle}
 exit
 """
     subprocess.Popen(['x-terminal-emulator', '-e',
@@ -63,15 +65,32 @@ exit
 
 def yukle(tur):
     konum = os.getcwd()
+    masaustu_ortami = os.environ.get('XDG_CURRENT_DESKTOP')
+    gnome_hata=""
+    if masaustu_ortami == "GNOME":
+        gnome_hata="""
+            sudo rm -f /usr/libexec/gnome-session-failed
+            sudo cp -f {konum}/gnome-session-failed /usr/libexec/
+            sudo chmod +x /usr/libexec/gnome-session-failed
+            """
     komut = f"""#!/usr/bin/bash
-
-sudo apt purge nvidi* xserver-xorg-video-nouveau -y
+echo 'İşlem bittikten sonra bilgisyar yeniden başlatılacaktır'
+echo 'Lütfen açık olan uygulamalarınızı kapatın'
+echo 'Lütfen şifrenizi giriniz'
+{gnome_hata}
+sudo apt update && sudo apt upgrade -y
+sleep 5
+sudo apt purge nvidia* -y
+sleep 5
 sudo apt install dirmngr ca-certificates software-properties-common apt-transport-https dkms curl unzip -y
+sleep 5
 curl -fSsL https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/3bf863cc.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/nvidia-drivers.gpg > /dev/null 2>&1
 echo 'deb [signed-by=/usr/share/keyrings/nvidia-drivers.gpg] https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/ /' | sudo tee /etc/apt/sources.list.d/nvidia-drivers.list
+sleep 5
 sudo add-apt-repository contrib
 sudo apt update
 sudo apt install sddm -y
+sleep 5
 """
     if tur == A_CV:
         komut += "sudo apt install nvidia-driver cuda nvidia-kernel-open-dkms nvidia-smi nvidia-settings -y"
@@ -81,6 +100,7 @@ sudo apt install sddm -y
         komut += "sudo apt install nvidia-driver cuda nvidia-smi nvidia-settings -y"
     else:
         komut += "sudo apt install nvidia-driver nvidia-smi nvidia-settings -y"
+    komut+="\nsudo apt purge xserver-xorg-video-nouveau -y \nsleep 5\nsudo reboot"
     print(komut)
     subprocess.Popen(['x-terminal-emulator', '-e',
                      f'bash -c "echo \'{komut}\' > betik.sh && chmod +x betik.sh && ./betik.sh; exec bash"'], cwd="/tmp/")
